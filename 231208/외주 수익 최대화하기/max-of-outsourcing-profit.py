b458af8794e2b8d5561a7dd51dcb4e036f47d3b4
JW-1210 <152844261+JW-1210@users.noreply.github.com>
2023-12-08 14:35:31 +0900

    Title: 외주 수익 최대화하기, Time: 152ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 231208/외주 수익 최대화하기/max-of-outsourcing-profit.py
@@ -0,0 +1,15 @@
+n = int(input())
+
+dp = [0 for _ in range(n+1)]
+arr = [list(map(int, input().split())) for _ in range(n)]
+
+for i in range(n-1, -1, -1):
+  # print(i)
+    if i + arr[i][0] <= n:
+        dp[i] = max(dp[i+arr[i][0]] + arr[i][1], dp[i+1])
+    else:
+        dp[i] = dp[i+1]
+
+    # print(dp)
+
+print(dp[0])
