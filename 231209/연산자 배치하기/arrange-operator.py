08b14822ffb64e519f0ad1463d8f200ad2a4e8d2
JW-1210 <152844261+JW-1210@users.noreply.github.com>
2023-12-09 16:03:04 +0900

    Title: 연산자 배치하기, Time: 130ms, Memory: 26MB, Status: Passed - CodeTree

--- 231209/연산자 배치하기/arrange-operator.py
+++ 231209/연산자 배치하기/arrange-operator.py
@@ -1,52 +1,23 @@
-from itertools import permutations
-
 n = int(input())
-# eq = [0 for _ in range(n+(n-1))]
-# print(eq)
-num = list(map(int,input().split())) #['1', '5', '3']
-func = ['+', '-', '*']
-func_num = list(map(int, input().split()))
-# print(func_num)
-func_count = []
 
-for i in range(len(func_num)):
-  if func_num[i] >= 1:
-    while func_num[i] > 0 :
-      func_count.append(func[i])
-      func_num[i] -= 1
-# print(func_count)
+num = list(map(int,input().split()))
+sum, sub, mul = map(int,input().split())
 
-func_perm = list(permutations(func_count, n-1))
-# print()
-# print(num[:-1])
-max_result = -10e8
-min_result = 10e8
+  
+def func(depth, total, sum, sub, mul):
+  global MIN, MAX
+  if depth == n:
+    MIN = min(total, MIN)
+    MAX = max(total, MAX)
 
-for fp in func_perm:
-  eq = []
-  sum = num[0]
-  # print(sum)
-  for i,j in zip(num[1:], fp):
-    # print(j, i)
-    sum = eval(str(sum)+str(j)+str(i))
+  if sum != 0:
     # print(sum)
-  max_result = max(sum, max_result)
-  min_result = min(sum, min_result)
-  
-  # if max_result >= 10e8:
-  #   print(10e8)
-  # elif min_result <= -10e8:
-  #   print(-10e8)
-# else:
-print(min_result, max_result)
-  # print(max_result, min_result)
-    # print(i,j)
-    
-    # result = eval()
-  
+    func(depth+1, total+num[depth], sum-1, sub, mul)
+  if sub != 0:
+    func(depth+1, total-num[depth], sum, sub-1, mul)
+  if mul != 0:
+    func(depth+1, total*num[depth], sum, sub, mul-1)
 
-
-
-# print(num)
-
-# for i in range(len(eq)):
+MIN,MAX = 10e8, -10e8
+func(1, num[0], sum, sub, mul)
+print(MIN, MAX)
